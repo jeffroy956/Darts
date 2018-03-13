cb6e88b8b3e5bc5b5fffe12da4cdb0630a87cb4a
Jeff Roy <jeffroy413@comcast.net>
2018-03-13 07:17:38 -0400

    testing push again

--- src/DartsApp.tsx
+++ src/DartsApp.tsx
@@ -3,6 +3,6 @@ import * as React from "react";
 export default class DartsApp extends React.Component<{}> {
 
     public render() {
-        return <div>Hello dart app! hot reload?!</div>;
+        return <div>Hello dart app!</div>;
     }
 }
