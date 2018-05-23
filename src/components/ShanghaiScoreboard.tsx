d767f04ca5f35595bf9d3995a674d12de467771c
Jeff Roy <jeffroy413@comcast.net>
2018-05-23 05:44:23 -0400

    add round highlighting to Shanghai

--- src/components/ShanghaiScoreboard.tsx
+++ src/components/ShanghaiScoreboard.tsx
@@ -22,14 +22,18 @@ export default class ShanghaiScoreboard extends React.Component<ShanghaiScoreboa
                             <th/>
                             <th/>
                             {playerScores[0].fieldScores.map((value, index) => 
-                                <th key={index}>{index + 1}</th>
+                                <th 
+                                    key={index} 
+                                    className={index === playerScores[0].turnNumber ? "target--active" : null}
+                                >{index + 1}
+                                </th>
                             )}
                             <th>Total</th>
                             <th/>
                         </tr>
                     </thead>
                     <tbody>
-                        {playerScores.map((ps) => 
+                        {playerScores.map((ps, index) => 
                             <ShanghaiPlayerScoreboard 
                                 key={ps.player.name} 
                                 shooterName={shooterName}
@@ -37,6 +41,7 @@ export default class ShanghaiScoreboard extends React.Component<ShanghaiScoreboa
                                 fieldScores={ps.fieldScores} 
                                 scoreTotal={ps.total}
                                 dartsThrown={ps.dartsThrown}
+                                turnNumber={ps.turnNumber}
                             />
                         )}
                     </tbody>
@@ -44,6 +49,7 @@ export default class ShanghaiScoreboard extends React.Component<ShanghaiScoreboa
             </div>
         );
     }
+
 }
 
 interface ShanghaiPlayerScoreboardProps {
@@ -52,19 +58,24 @@ interface ShanghaiPlayerScoreboardProps {
     fieldScores: number[];
     scoreTotal: number;
     dartsThrown: number;
+    turnNumber: number;
 }
 
 // tslint:disable-next-line:max-classes-per-file
 export class ShanghaiPlayerScoreboard extends React.Component<ShanghaiPlayerScoreboardProps> {
     public render() {
-        const {playerName, fieldScores, scoreTotal, dartsThrown} = this.props;
+        const {playerName, fieldScores, scoreTotal, dartsThrown, turnNumber} = this.props;
         const isActive = playerName === this.props.shooterName;
         return(
             <tr className={isActive ? "player--active" : ""}>
                 <td className="player-indicator"><i className="material-icons">chevron_right</i></td>
                 <td className="player-name"><span>{playerName}</span><span>({dartsThrown})</span></td>
                 {fieldScores.map((fs, index) => 
-                    <td key={index} className="field-score--narrow">{fs}</td>
+                    <td 
+                        key={index} 
+                        className={"field-score--narrow" + (turnNumber === index ? " target--active" : "")}
+                    >{fs}
+                    </td>
                 )}
                 <td className="field-score--total">{scoreTotal}</td>
                 <td className="player-indicator"><i className="material-icons">chevron_left</i></td>
