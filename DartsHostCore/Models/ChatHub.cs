2d6e5c72377b1298b98fe508304e740e827102d4
Jeff Roy <jeffroy413@comcast.net>
2018-05-28 09:05:54 -0400

    basic .NET core mvc shell with signal R

---
+++ DartsHostCore/Models/ChatHub.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.SignalR;
+
+using System.Threading.Tasks;
+
+namespace DartsHostCore
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user,message);
+        }
+    }
+}
